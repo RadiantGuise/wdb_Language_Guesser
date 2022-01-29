5527e067cbae7cc980a6c066098dc30cde2f0353
RadiantGuise <RadiantGuise@protonmail.com>
2022-01-29 17:26:18 -0600

    Add ability to take input from user in CLI

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import {franc} from 'franc';
 import langs from 'langs';
+const input = process.argv[2];
 
-const langCode = franc('Alle menslike wesens word vry');
+const langCode = franc(input);
 
 const language = langs.where('3', langCode);
 
