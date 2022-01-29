736fb2d0861e7f46e1a35b24f5ca1ccaf7a38fd2
RadiantGuise <RadiantGuise@protonmail.com>
2022-01-29 17:33:25 -0600

    Add basic error handling

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-import {franc} from 'franc';
+import {franc, francAll} from 'franc';
 import langs from 'langs';
 const input = process.argv[2];
-
 const langCode = franc(input);
 
-const language = langs.where('3', langCode);
+if (langCode === 'und') {
+    console.log("Sorry, couldn't figure it out! Try with more sample text!");
+} else {
+    const language = langs.where('3', langCode);
+    console.log(`Our best guess is: ${language.name}`);
+}
 
-console.log(language.name);
+
